9c4538d44d5f25ea575bf211d7dadf950b7a918d
estrellaruisz <edavidcristobal@gmail.com>
2023-01-14 13:09:42 -0500

    first commit

---
+++ Solucion.py
@@ -0,0 +1,11 @@
+print("Solucion problema 6")
+while True:
+    num=int(input("Digite el numero : "))
+    if num>0 and num<999 :
+        print("El numero tiene", len(str(num)),"digitos" )
+    if num==0:
+        break 
+    print("Programa finalizado")
+
+
+
